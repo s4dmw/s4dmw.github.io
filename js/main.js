bf45b85cc60e857f48f3c4e4c8aa777b5221ec01
David <dmwhite@gmail.com>
2015-11-26 09:11:35 -0500

    set up routes

--- js/main.js
+++ js/main.js
@@ -7,6 +7,22 @@ console.log("main.js")
           templateUrl: 'partials/landing.html',
           controller: 'MainController'
         })
+        .when('/about', {
+          templateUrl: 'partials/about.html',
+          controller: 'MainController'
+        })
+        .when('/portfolio', {
+          templateUrl: 'partials/projects.html',
+          controller: 'MainController'
+        })
+        .when('/resume', {
+          templateUrl: 'partials/resume.html',
+          controller: 'MainController'
+        })
+        .when('/contact', {
+          templateUrl: 'partials/contact.html',
+          controller: 'MainController'
+        })
 
   }) //end angular.module
 
